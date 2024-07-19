62802ef28081aa65093c758d0f57bc9577765446
Stephen Chudleigh <stephen@smartlogic.io>
2024-07-19 13:53:17 -0700

    add annotate gem and user schema documentation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,34 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                         :bigint           not null, primary key
+#  email                      :string(255)      not null
+#  password_hash              :string(255)
+#  token                      :uuid
+#  first_name                 :string(255)
+#  last_name                  :string(255)
+#  phone_number               :string(255)
+#  inserted_at                :datetime         not null
+#  updated_at                 :datetime         not null
+#  email_verification_token   :string(255)
+#  email_verified_at          :datetime
+#  role                       :string(255)      default("user"), not null
+#  password_reset_token       :uuid
+#  password_reset_expires_at  :datetime
+#  finalized                  :boolean          default(TRUE), not null
+#  avatar_key                 :uuid
+#  avatar_extension           :string(255)
+#  display                    :boolean          default(TRUE), not null
+#  terms_of_use               :datetime
+#  privacy_guidelines         :datetime
+#  agency_id                  :bigint
+#  last_active                :datetime
+#  status                     :string(255)
+#  active_session             :boolean          default(FALSE)
+#  renewal_request            :string(255)
+#  jwt_token                  :text
+#  recertification_expired_at :datetime
+#
 class User < ApplicationRecord
 end
