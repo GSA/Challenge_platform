6173cececed148f688ded5417d158b750381c779
Stone Filipczak <stone@smartlogic.io>
2024-08-28 09:17:18 -0400

    address what linter believes to be duplicated code blocks

--- spec/support/shared_examples/utility_menu.rb
+++ spec/support/shared_examples/utility_menu.rb
@@ -1,5 +1,5 @@
 RSpec.shared_examples "a page with utility menu links for a public solver" do
-    it "has the right links" do
+    it "has the right links for a public solver" do
         expect(response.body).to include("Dashboard")
         expect(response.body).to include("Submissions")
         expect(response.body).to include("User Guides")
@@ -8,7 +8,7 @@ RSpec.shared_examples "a page with utility menu links for a public solver" do
 end
 
 RSpec.shared_examples "a page with utility menu links for a challenge manager" do
-    it "has the right links" do
+    it "has the right links for a challenge manager" do
         expect(response.body).to include("Dashboard")
         expect(response.body).to include("Challenges")
         expect(response.body).to include("Submissions")
@@ -19,7 +19,7 @@ RSpec.shared_examples "a page with utility menu links for a challenge manager" d
 end   
 
 RSpec.shared_examples "a page with utility menu links for an evaluator" do
-    it "has the right links" do
+    it "has the right links for an evaluator" do
         expect(response.body).to include("Dashboard")
         expect(response.body).to include("Evaluations")
         expect(response.body).to include("User Guides")
