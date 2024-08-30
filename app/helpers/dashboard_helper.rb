517e55176fcc71e095fecdd40c192d74727e78eb
Stone Filipczak <stone@smartlogic.io>
2024-08-30 15:50:48 -0400

    reorder

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -11,10 +11,10 @@ module DashboardHelper
   def card_data
     {
       challenge_manager: [
-        { image_path: 'content_copy', href: 'evaluation_forms', alt: 'evaluation forms', title: 'Evaluation Forms',
-          subtitle: 'Create and manage evaluation forms.' },
         { image_path: 'folder_open', href: 'manage_submissions', alt: 'submissions', title: 'Submissions',
           subtitle: 'Manage submissions, evaluations, and evaluators.' },
+        { image_path: 'content_copy', href: 'evaluation_forms', alt: 'evaluation forms', title: 'Evaluation Forms',
+          subtitle: 'Create and manage evaluation forms.' },
         { image_path: 'map', href: 'user_guide', alt: 'user guides', title: 'User Guides',
           subtitle: 'Learn how to make the most of the platform.' },
         { image_path: 'support_agent', href: 'federal-agency-faqs', alt: 'help', title: 'Help',
