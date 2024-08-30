835d60154968048841d1ca86e60a096fc151cdbb
Stone Filipczak <stone@smartlogic.io>
2024-08-30 15:49:21 -0400

    rubocop

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -1,30 +1,33 @@
 # frozen_string_literal: true
 
 module DashboardHelper
-  
   def dashboard(user)
     role = user.role.to_sym
     cards = card_data[role]
     title = dashboard_title[role]
-    render partial: "dashboard", locals: {cards: , title:}
+    render partial: "dashboard", locals: { cards:, title: }
   end
 
   def card_data
     {
-      "challenge_manager": [
-        {image_path: 'content_copy', href: 'evaluation_forms', alt: 'evaluation forms', title: 'Evaluation Forms', subtitle: 'Create and manage evaluation forms.'},
-        {image_path: 'folder_open', href: 'manage_submissions', alt: 'submissions', title: 'Submissions', subtitle: 'Manage submissions, evaluations, and evaluators.'},
-        {image_path: 'map', href: 'user_guide', alt: 'user guides', title: 'User Guides', subtitle: 'Learn how to make the most of the platform.'},
-        {image_path: 'support_agent', href: 'federal-agency-faqs', alt: 'help', title: 'Help', subtitle: 'Get support on the Challenge.Gov platform.'}
+      challenge_manager: [
+        { image_path: 'content_copy', href: 'evaluation_forms', alt: 'evaluation forms', title: 'Evaluation Forms',
+          subtitle: 'Create and manage evaluation forms.' },
+        { image_path: 'folder_open', href: 'manage_submissions', alt: 'submissions', title: 'Submissions',
+          subtitle: 'Manage submissions, evaluations, and evaluators.' },
+        { image_path: 'map', href: 'user_guide', alt: 'user guides', title: 'User Guides',
+          subtitle: 'Learn how to make the most of the platform.' },
+        { image_path: 'support_agent', href: 'federal-agency-faqs', alt: 'help', title: 'Help',
+          subtitle: 'Get support on the Challenge.Gov platform.' }
       ],
-      "solver": [],
-      "evaluator": []
+      solver: [],
+      evaluator: []
     }
   end
 
   def dashboard_title
     {
-      "challenge_manager": "Challenge Manager Evaluation Dashboard"
+      challenge_manager: "Challenge Manager Evaluation Dashboard"
     }
-  end  
+  end
 end
