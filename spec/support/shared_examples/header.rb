39dee1e58f9cd0d2a24a3715c1eca2e1c5b9c719
Stone Filipczak <51140224+stonefilipczak@users.noreply.github.com>
2024-09-03 11:36:41 -0400

    [#25,#5, #27] Header / Nav / Banner / Utility Menu (#124)
    * government banner
    * header
    * mobile nav
    
    Co-authored-by: Stephen Chudleigh <stepchud@users.noreply.github.com>
    
    * shared specs for utility menu
    * increase threshold for similar code
    
    ---------
    
    Co-authored-by: Stephen Chudleigh <stepchud@users.noreply.github.com>
    Co-authored-by: Stephen Chudleigh <stephen@smartlogic.io>

---
+++ spec/support/shared_examples/header.rb
@@ -0,0 +1,8 @@
+RSpec.shared_examples "a page with header content" do
+  it "has a header with the expected links" do
+    expect(response.body).to include("Find a Challenge")
+    expect(response.body).to include("Resources")
+    expect(response.body).to include("Events")
+    expect(response.body).to include("Contact")
+  end
+end
