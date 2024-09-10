1643f90cef53b6bc9dfcf1103b79573b954dd8e5
Stone Filipczak <stone@smartlogic.io>
2024-09-10 10:03:01 -0400

    evaluator dashboard

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -6,22 +6,30 @@ module DashboardHelper
       challenge_manager: [
         { image_path: 'emoji_events', href: 'http://localhost:4000/', alt: 'challenges', title: 'Challenges',
           subtitle: 'Create and manage challenges.' },
-        { image_path: 'folder_open', href: 'manage_submissions', alt: 'submissions', title: 'Submissions',
+        { image_path: 'people', href: 'manage_submissions', alt: 'submissions', title: 'Submissions',
           subtitle: 'Manage submissions, evaluations, and evaluators.' },
         { image_path: 'content_copy', href: 'evaluation_forms', alt: 'evaluation forms', title: 'Evaluation Forms',
           subtitle: 'Create and manage evaluation forms.' },
-        { image_path: 'map', href: 'user_guide', alt: 'user guides', title: 'User Guides',
+        { image_path: 'map', href: 'user_guide', alt: 'resources', title: 'Resources',
           subtitle: 'Learn how to make the most of the platform.' },
         { image_path: 'support_agent', href: 'federal-agency-faqs', alt: 'help', title: 'Help',
           subtitle: 'Get support on the Challenge.Gov platform.' }
       ],
-      evaluator: []
+      evaluator: [
+        { image_path: 'content_copy', href: 'manage_submissions', alt: 'submissions', title: 'Submissions',
+          subtitle: 'Evaluate my assigned submissions.' },
+        { image_path: 'map', href: 'user_guide', alt: 'user guides', title: 'Resources',
+          subtitle: 'Learn how to make the most of the platform.' },
+        { image_path: 'support_agent', href: 'federal-agency-faqs', alt: 'help', title: 'Help',
+          subtitle: 'Get support on the Challenge.Gov platform.' }
+      ],
     }
   end
 
   def dashboard_title
     {
-      challenge_manager: "Challenge Manager Evaluation Dashboard"
+      challenge_manager: "Challenge Manager Evaluation Dashboard",
+      evaluator: "Evaluator Dashboard"
     }
   end
 end
