db7449d32244a8b8e3956809af88df9fec5c4584
Stone Filipczak <stone@smartlogic.io>
2024-09-26 10:45:11 -0400

    fix up specs

--- spec/requests/dashboard_request_spec.rb
+++ spec/requests/dashboard_request_spec.rb
@@ -9,16 +9,6 @@ RSpec.describe "DashboardController" do
     it_behaves_like "a page with footer content"
     it_behaves_like "a page with header content"
 
-    context "when logged in as public solver on the root url" do
-      before do
-        user.update(role: "solver")
-        get "/"
-      end
-
-      it_behaves_like "a page with utility menu links for all users"
-      it_behaves_like "a page with utility menu links for a public solver"
-    end
-
     context "when logged in as a challenge manager on the root url" do
       before do
         user.update(role: "challenge_manager")
@@ -37,7 +27,6 @@ RSpec.describe "DashboardController" do
       end
 
       it_behaves_like "a page with utility menu links for all users"
-      it_behaves_like "a page with utility menu links for an evaluator"
       it_behaves_like "a page with dashboard content for an evaluator"
     end
   end
@@ -47,16 +36,6 @@ RSpec.describe "DashboardController" do
 
     it_behaves_like "a page with footer content"
     it_behaves_like "a page with header content"
-
-    context "when logged in as a public solver on the dashboard" do
-      before do
-        user.update(role: "solver")
-        get "/dashboard"
-      end
-
-      it_behaves_like "a page with utility menu links for all users"
-      it_behaves_like "a page with utility menu links for a public solver"
-    end
 
     context "when logged in as a challenge manager on the dashboard" do
       before do
@@ -76,7 +55,6 @@ RSpec.describe "DashboardController" do
       end
 
       it_behaves_like "a page with utility menu links for all users"
-      it_behaves_like "a page with utility menu links for an evaluator"
       it_behaves_like "a page with dashboard content for an evaluator"
     end
   end
--- spec/support/shared_examples/utility_menu.rb
+++ spec/support/shared_examples/utility_menu.rb
@@ -1,27 +1,14 @@
 RSpec.shared_examples "a page with utility menu links for all users" do
   it "has links shared across all user roles" do
     expect(response.body).to include("Dashboard")
-    expect(response.body).to include("User Guides")
-    expect(response.body).to include("Help")
-  end
-end
-
-RSpec.shared_examples "a page with utility menu links for a public solver" do
-  it "has the right links for a public solver" do
     expect(response.body).to include("Submissions")
+    expect(response.body).to include("Resources")
   end
 end
 
 RSpec.shared_examples "a page with utility menu links for a challenge manager" do
   it "has the right links for a challenge manager" do
     expect(response.body).to include("Challenges")
-    expect(response.body).to include("Submissions")
-    expect(response.body).to include("Evaluations")
-  end
-end
-
-RSpec.shared_examples "a page with utility menu links for an evaluator" do
-  it "has the right links for an evaluator" do
-    expect(response.body).to include("Evaluations")
+    expect(response.body).to include("Evaluation Forms")
   end
 end
