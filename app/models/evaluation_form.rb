0c4410d2f294432c1e7082a3740bdc2421487f12
Stone Filipczak <stone@smartlogic.io>
2024-09-26 18:03:52 -0400

    remove status and publication date from views

--- app/models/evaluation_form.rb
+++ app/models/evaluation_form.rb
@@ -3,8 +3,5 @@
 class EvaluationForm < ApplicationRecord
   belongs_to :challenge
 
-
-  validates :title, presence: true
-  validates :instructions, presence: true
   validates :challenge_phase, presence: true
 end
