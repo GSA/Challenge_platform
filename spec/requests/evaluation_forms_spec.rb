0cd1d6ef644d1a3114d723eb8298b8b5b021f416
Stone Filipczak <stone@smartlogic.io>
2024-09-27 08:59:38 -0400

    evaluation form request specs

--- spec/requests/evaluation_forms_spec.rb
+++ spec/requests/evaluation_forms_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe "EvaluationForms" do
-  before { create_and_log_in_user(role: "challenge_manager") }
+  before { @user = create_and_log_in_user(role: "challenge_manager") }
 
   describe "GET /evaluation_forms" do
     it "renders the index view with the correct header" do
@@ -15,12 +15,35 @@ RSpec.describe "EvaluationForms" do
       expect(response.body).to include("You currently do not have any evaluation forms.")
     end
     
-    it "renders a list of evaluation forms" do
-      EvaluationForm.create!(title: "Frodo")
-      EvaluationForm.create!(title: "Sam")
+    it "renders a list of evaluation forms for the current user's challenges" do
+      agency = Agency.create!(name: "Gandalf and Sons", acronym: "GAD")
+      challenge = Challenge.create!(user: @user, agency: agency, title: "Turning red bull into water")
+      challenge_manager = ChallengeManager.create(user: @user, challenge: challenge)
+
+      EvaluationForm.create!(title: "Frodo", challenge_id: challenge.id)
+      EvaluationForm.create!(title: "Sam", challenge_id: challenge.id)
       get evaluation_forms_path
-      expect(response.body).to include("Frodo")
       expect(response.body).to include("Sam")
-    end  
+      expect(response.body).to include("Frodo")
+    end
+    
+    it "does not include evaluation forms for challenges not assigned to the current user" do
+      agency = Agency.create!(name: "Gandalf and Sons", acronym: "GAD")
+      challenge = Challenge.create!(user: @user, agency: agency, title: "Turning monster energy into chamomile")
+      challenge_manager = ChallengeManager.create(user: @user, challenge: challenge)
+
+      user_2 = create_user(role: "challenge_manager", email: "testwizard@example.gov")
+      challenge_2 = Challenge.create!(user: user_2, agency: agency, title: "Turning frogs into princes")
+      challenge_manager_2 = ChallengeManager.create(user: user_2, challenge: challenge)
+
+      EvaluationForm.create!(title: "Shrek", challenge_id: challenge.id)
+      EvaluationForm.create!(title: "Fiona", challenge_id: challenge.id)
+      EvaluationForm.create!(title: "Donkey", challenge_id: challenge_2.id)
+      EvaluationForm.create!(title: "Farquad", challenge_id: challenge_2.id)
+
+      get evaluation_forms_path
+      expect(response.body).to include("Shrek")
+      expect(response.body).to include("Fiona")
+    end
   end
 end
