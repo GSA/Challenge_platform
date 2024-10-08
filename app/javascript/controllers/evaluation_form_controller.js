4281daf8e77dae44335668ceb9f0104960a49255
Stone Filipczak <stone@smartlogic.io>
2024-10-08 12:28:49 -0400

    javascript refactor

--- app/javascript/controllers/evaluation_form_controller.js
+++ app/javascript/controllers/evaluation_form_controller.js
@@ -8,25 +8,34 @@ export default class extends Controller {
     let id, phase, end_date
     [id, phase, end_date] = e.target.value.split(".")
     if (id) {
+      // set values of hidden form fields 
+      this.challengeIDTarget.value = id
+      this.challengePhaseTarget.value = phase
 
+      // set the start date of the evaluation form 
+      // to be the challenge's end date
+      this.startDateTarget.innerHTML = end_date || "mm/dd/yyyy"
+
+      this.updateErrorMessage("evaluation_form_challenge_id", "")
+      this.updateErrorMessage("evaluation_form_challenge_phase", "")
+    } else {
+      this.updateErrorMessage("evaluation_form_challenge_id", "can't be blank")
+      this.startDateTarget.innerHTML = "mm/dd/yyyy"
     }
-
-    // set values of hidden form fields 
-    this.challengeIDTarget.value = id
-    this.challengePhaseTarget.value = phase
-
-    // set the start date of the evaluation form 
-    // to be the challenge's end date
-    this.startDateTarget.innerHTML = end_date || "mm/dd/yyyy"
   }
 
   validatePresence(e) {
     if (!e.target.value) {
       e.target.classList.add("border-secondary")
-      document.getElementById(e.target.id + "_error").innerHTML = "can't be blank"
+      this.updateErrorMessage(e.target.id, "can't be blank")
+
     } else {
       e.target.classList.remove("border-secondary")
-      document.getElementById(e.target.id + "_error").innerHTML = ""
+      this.updateErrorMessage(e.target.id, "")
     }
+  }
+
+  updateErrorMessage(field, message) {
+    document.getElementById(field + "_error").innerHTML = message
   }
 }
