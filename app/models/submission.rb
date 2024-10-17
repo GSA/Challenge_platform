b594158c4bb5ffe0ec442162421cf24c0e50802d
Stone Filipczak <stone@smartlogic.io>
2024-10-17 09:53:12 -0400

    make status and judging status into enums

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 class Submission < ApplicationRecord
+  enum :status, { draft: 0, submitted: 1 }
+  enum :judging_status, { not_selected: 0, selected: 1, qualified: 2, winner: 3 }
+
   # Associations
   belongs_to :submitter, class_name: 'User'
   belongs_to :challenge
@@ -12,11 +15,8 @@ class Submission < ApplicationRecord
   attribute :brief_description, :string
   attribute :description, :string
   attribute :external_url, :string
-  attribute :status, :string
-  attribute :judging_status, :string, default: "not_selected"
   attribute :terms_accepted, :boolean, default: nil
   attribute :review_verified, :boolean, default: nil
-
 
   # Validations
   validates :title, presence: true
