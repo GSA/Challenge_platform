42b838e4806f6f23b6b7d38b94dc8d2274a3e6dd
Stone Filipczak <stone@smartlogic.io>
2024-10-22 12:26:00 -0400

    update markup

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -7,7 +7,7 @@ module DashboardHelper
         { image_path: 'emoji_events', href: Rails.configuration.phx_interop[:phx_uri],
           alt: 'challenges', title: 'Challenges', subtitle: 'Create and manage challenges.' },
         { image_path: 'star_half', href: 'manage_submissions',
-          alt: 'submissions', title: 'Submissions', subtitle: 'Manage submissions, evaluations, and evaluators.' },
+          alt: 'submissions and evaluations', title: 'Submissions & Evaluations', subtitle: 'Manage submissions, evaluations, and evaluators.' },
         { image_path: 'check_circle_outline', href: 'evaluation_forms',
           alt: 'evaluation forms', title: 'Evaluation Forms', subtitle: 'Create and manage evaluation forms.' },
         { image_path: 'mail', href: "#{Rails.configuration.phx_interop[:phx_uri]}/messages",
@@ -19,7 +19,7 @@ module DashboardHelper
       ],
       evaluator: [
         { image_path: 'star_half', href: 'evaluations',
-          alt: 'submissions', title: 'Submissions',
+          alt: 'submissions and evaluations', title: 'Submissions & Evaluations',
           subtitle: 'View submissions assigned to me and provide evaluations.' },
         { image_path: 'support', href: 'https://www.challenge.gov/cm-user-guide/',
           alt: 'resources', title: 'Resources', subtitle: 'Learn how to make the most of the platform.' }
--- app/helpers/navigation_helper.rb
+++ app/helpers/navigation_helper.rb
@@ -4,7 +4,7 @@ module NavigationHelper
   def utility_menu_link(image_path, href, alt, button_label)
     link_to(href,
             class: "display-flex flex-align-center flex-column desktop:flex-row " \
-                   "margin-x-1 desktop:margin-x-3 text-white text-no-wrap") do
+                   "margin-x-1 desktop:margin-x-3 text-white width-9 tablet:width-auto tablet:text-no-wrap text-center") do
       image_tag(
         "images/usa-icons/#{image_path}.svg",
         class: "usa-icon--size-4 desktop:usa-icon--size-3 icon-white desktop:margin-right-1",
