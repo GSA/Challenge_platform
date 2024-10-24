05051ba535ae0d9d8686d352468cf11a33e633e1
Stephen Chudleigh <stephen@smartlogic.io>
2024-10-24 15:28:25 -0700

    fix the migrations on challenge-dev

--- db/migrate/20241001143033_change_null_values_on_evaluation_forms.rb
+++ db/migrate/20241001143033_change_null_values_on_evaluation_forms.rb
@@ -1,5 +1,6 @@
 class ChangeNullValuesOnEvaluationForms < ActiveRecord::Migration[7.2]
   def change
+    ActiveRecord::Base.connection.truncate_tables(EvaluationForm.table_name)
     change_column_null :evaluation_forms, :title, false
     change_column_null :evaluation_forms, :instructions, false
     change_column_null :evaluation_forms, :challenge_phase, false
