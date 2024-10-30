f0eb1238d37e6199fb95ee9b6127e1d40173e874
Stephen Chudleigh <stephen@smartlogic.io>
2024-10-30 09:12:44 -0700

    codeclimate nested classes

--- app/controllers/dev/sandbox_controller.rb
+++ app/controllers/dev/sandbox_controller.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
-class Dev::SandboxController < ApplicationController
-  def index; end
+module Dev
+  class SandboxController < ApplicationController
+    def index; end
+  end
 end
