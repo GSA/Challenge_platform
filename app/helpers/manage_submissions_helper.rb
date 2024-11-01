a328be189abef1566f19798a381d6f0eca117347
Stone Filipczak <stone@smartlogic.io>
2024-11-01 11:30:35 -0400

    helper methods

--- app/helpers/manage_submissions_helper.rb
+++ app/helpers/manage_submissions_helper.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 module ManageSubmissionsHelper
+    def eligible_for_evaluation?(submission)
+      submission.judging_status.in? ["qualified"]
+    end 
+
+    def selected_to_advance?(submission)
+      submission.judging_status.in? ["selected", "winner"]
+    end   
 end
