145886aad05ed530dc67e15dfda8ac63a3e84e1f
Emma Whamond <emma@smartlogic.io>
2024-11-07 12:25:24 -0600

    179 | Add evaluation status order

--- app/models/evaluator_submission_assignment.rb
+++ app/models/evaluator_submission_assignment.rb
@@ -4,5 +4,26 @@ class EvaluatorSubmissionAssignment < ApplicationRecord
   belongs_to :submission
   belongs_to :evaluator, class_name: "User", foreign_key: :user_id, inverse_of: :assigned_submissions
 
-  enum :status, { assigned: 0, unassigned: 1, recused: 2 }
+  has_one :phase, through: :submission
+
+  enum :status, {
+    assigned: 0,
+    unassigned: 1,
+    recused: 2,
+    not_started: 3,
+    in_progress: 4,
+    completed: 5
+  }
+
+  STATUS_ORDER = [:recused, :not_started, :in_progress, :completed]
+
+  scope :ordered_by_status, -> {
+    order(Arel.sql(
+      "CASE " +
+      STATUS_ORDER.map.with_index { |status, index|
+        "WHEN evaluator_submission_assignments.status = #{statuses[status]} THEN #{index + 1}"
+      }.join(" ") +
+      " ELSE #{STATUS_ORDER.length + 1} END"
+    ))
+  }
 end
