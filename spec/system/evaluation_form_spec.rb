19efafe78f3907e0b2797c5d6f6b17d771184095
Chris Preisinger <cpreisinger@gmail.com>
2024-11-11 02:19:10 -0500

    248 Eval form spec helper functions

--- spec/system/evaluation_form_spec.rb
+++ spec/system/evaluation_form_spec.rb
@@ -22,42 +22,45 @@ RSpec.describe 'Evaluation Form', :js, type: :system do
     it 'allows creation of a valid form' do
       visit new_evaluation_form_path
 
-      # Fill in title
-      fill_in 'evaluation_form[title]', with: 'New Evaluation Form'
+      # Fill in main form fields
+      fill_in_title("New Evaluation Form")
+      select_phase(challenge.phases.first)
+      fill_in_instructions("Example instructions")
+      check_require_comments
+      select_scale_type("point")
+      fill_in_end_date(challenge.phases.first.end_date + 1)
 
-      # Select phase
-      phase = challenge.phases.first
-      challenge_phase_title = challenge_phase_title(challenge, phase)
-      find_by_id('challenge-combo').click
-      find('#challenge-combo--list li', text: challenge_phase_title).click
+      # Fill in initial criterion
+      index = 0
+      fill_in_criterion_title(index, "New Numeric Evaluation Criterion")
+      fill_in_criterion_description(index, "Example criterion description")
+      fill_in_criterion_points_weight(index, "10")
+      select_criterion_scoring_type(index, "numeric")
 
-      # Fill in instructions
-      fill_in 'evaluation_form[instructions]', with: 'Example instructions'
+      # Add new criterion (binary scoring type) and fill in
+      index = add_criterion
+      fill_in_criterion_title(index, "New Binary Evaluation Criterion")
+      fill_in_criterion_description(index, "Example criterion description")
+      fill_in_criterion_points_weight(index, "10")
+      select_criterion_scoring_type(index, "binary")
+      fill_in_criterion_option_label(index, 0, "No")
+      fill_in_criterion_option_label(index, 1, "Yes")
 
-      # Check required comments box
-      find("label[for='evaluation_form_comments_required']").click
+      # Add new criterion (rating scoring type) and fill in
+      index = add_criterion
+      fill_in_criterion_title(index, "New Rating Evaluation Criterion")
+      fill_in_criterion_description(index, "Example criterion description")
+      fill_in_criterion_points_weight(index, "10")
+      select_criterion_scoring_type(index, "rating")
+      select_option_range_start(index, 1)
+      select_option_range_end(index, 5)
+      fill_in_criterion_option_label(index, 1, "Disagree")
+      fill_in_criterion_option_label(index, 2, "Slightly Disagree")
+      fill_in_criterion_option_label(index, 3, "Neutral")
+      fill_in_criterion_option_label(index, 4, "Slightly Agree")
+      fill_in_criterion_option_label(index, 5, "Agree")
 
-      # Select scale type radio button
-      find("label[for='point_scale']").click
-
-      # Fill in initial criteria title
-      fill_in 'evaluation_form[evaluation_criteria_attributes][0][title]', with: 'New Evaluation Criteria'
-
-      # Fill in initial criteria description
-      fill_in 'evaluation_form[evaluation_criteria_attributes][0][description]', with: 'Example criteria description'
-
-      # Fill in initial criteria points/weight
-      fill_in 'evaluation_form[evaluation_criteria_attributes][0][points_or_weight]', with: '10'
-
-      # Fill in initial criteria scoring type
-      find("label[for='evaluation_form_evaluation_criteria_attributes_0_scoring_type_numeric']").click
-
-      # Fill in end date with datepicker
-      find(".usa-date-picker__button").click
-      date = (phase.end_date + 1).strftime("%Y-%m-%d")
-      find(".usa-date-picker__calendar__date[data-value='#{date}']").click
-
-      click_on 'Save'
+      save_form
 
       expect(page).to have_content('Evaluation Form Saved')
     end
@@ -79,4 +82,92 @@ RSpec.describe 'Evaluation Form', :js, type: :system do
       expect(page).to(be_axe_clean)
     end
   end
+end
+
+# Form Fill Helpers
+def fill_in_title(value)
+  fill_in 'evaluation_form[title]', with: value
+end
+
+def select_phase(phase)
+  challenge_phase_title = challenge_phase_title(phase.challenge, phase)
+  find_by_id('challenge-combo').click
+  find('#challenge-combo--list li', text: challenge_phase_title).click
+end
+
+def fill_in_instructions(value)
+  fill_in 'evaluation_form[instructions]', with: value
+end
+
+def check_require_comments
+  find("label[for='evaluation_form_comments_required']").click
+end
+
+def select_scale_type(scale_type)
+  allowed_scale_types = %w[point weight]
+  unless allowed_scale_types.include?(scale_type)
+    raise ArgumentError, "Invalid scale type: #{scale_type}. Allowed values are: #{allowed_scale_types.join(', ')}"
+  end
+
+  find("label[for='#{scale_type}_scale']").click
+end
+
+def fill_in_criterion_title(index, value)
+  fill_in "evaluation_form[evaluation_criteria_attributes][#{index}][title]", with: value
+end
+
+def fill_in_criterion_description(index, value)
+  fill_in "evaluation_form[evaluation_criteria_attributes][#{index}][description]", with: value
+end
+
+def fill_in_criterion_points_weight(index, value)
+  fill_in "evaluation_form[evaluation_criteria_attributes][#{index}][points_or_weight]", with: value
+end
+
+def select_criterion_scoring_type(index, scoring_type)
+  allowed_scoring_types = %w[numeric rating binary]
+  unless allowed_scoring_types.include?(scoring_type)
+    raise ArgumentError,
+          "Invalid scoring type: #{scoring_type}. Allowed values are: #{allowed_scoring_types.join(', ')}"
+  end
+
+  find("label[for='evaluation_form_evaluation_criteria_attributes_#{index}_scoring_type_#{scoring_type}']").click
+end
+
+def add_criterion
+  click_link_or_button "add-criteria-button"
+  # Returns the last visible criterion index (most recently added)
+  visible_criterion_indicies[-1]
+end
+
+def remove_criterion(index)
+  click_link_or_button "evaluation_form_evaluation_criteria_attributes_#{index}_delete_criteria"
+end
+
+def visible_criterion_indicies
+  all('.criteria-row').map.with_index { |_element, index| index }
+end
+
+def select_option_range_start(index, value)
+  select value, from: "evaluation_form[evaluation_criteria_attributes][#{index}][option_range_start]"
+end
+
+def select_option_range_end(index, value)
+  select value, from: "evaluation_form[evaluation_criteria_attributes][#{index}][option_range_end]"
+end
+
+def fill_in_criterion_option_label(criterion_index, label_index, value)
+  fill_in "evaluation_form[evaluation_criteria_attributes][#{criterion_index}][option_labels][#{label_index}]",
+          with: value
+end
+
+# TODO: This might need to go a month ahead if the date isn't found initially
+def fill_in_end_date(date)
+  date = date.strftime("%Y-%m-%d")
+  find(".usa-date-picker__button").click
+  find(".usa-date-picker__calendar__date[data-value='#{date}']").click
+end
+
+def save_form
+  click_link_or_button 'Save'
 end
