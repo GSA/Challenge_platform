9cffa670be77c58f90b51e9f33cba19bfe09f374
Emma Whamond <emma@smartlogic.io>
2024-11-15 13:47:52 -0600

    Use the counter_cache

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -6,7 +6,7 @@ class Submission < ApplicationRecord
 
   # Associations
   belongs_to :challenge
-  belongs_to :phase
+  belongs_to :phase, counter_cache: true
   belongs_to :submitter, class_name: 'User'
   belongs_to :manager, class_name: 'User'
   has_many :evaluator_submission_assignments, dependent: :destroy
