32861409702921deebabdf99b9f29595301933ed
Emma Whamond <emma@smartlogic.io>
2024-11-19 13:30:07 -0600

    revert structure.sql changes

--- db/structure.sql
+++ db/structure.sql
@@ -10,13 +10,6 @@ SET client_min_messages = warning;
 SET row_security = off;
 
 --
--- Name: public; Type: SCHEMA; Schema: -; Owner: -
---
-
--- *not* creating schema, since initdb creates it
-
-
---
 -- Name: oban_job_state; Type: TYPE; Schema: public; Owner: -
 --
 
@@ -692,7 +685,7 @@ CREATE TABLE public.oban_jobs (
     attempted_by text[],
     discarded_at timestamp without time zone,
     priority integer DEFAULT 0 NOT NULL,
-    tags character varying(255)[] DEFAULT ARRAY[]::character varying[],
+    tags text[] DEFAULT ARRAY[]::text[],
     meta jsonb DEFAULT '{}'::jsonb,
     cancelled_at timestamp without time zone,
     CONSTRAINT attempt_range CHECK (((attempt >= 0) AND (attempt <= max_attempts))),
