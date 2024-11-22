f0013f6bf3e22bf2caa3cb1a28a47590e4968d42
Emma Whamond <emma@smartlogic.io>
2024-11-22 15:31:51 -0600

    179 | Adjust SQL statement for status order

--- app/models/evaluator_submission_assignment.rb
+++ app/models/evaluator_submission_assignment.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 class EvaluatorSubmissionAssignment < ApplicationRecord
-  include ActiveRecord::Sanitization
-
   belongs_to :submission
   belongs_to :evaluator, class_name: "User", foreign_key: :user_id, inverse_of: :assigned_submissions
   has_one :evaluation, dependent: :destroy
@@ -19,16 +17,18 @@ class EvaluatorSubmissionAssignment < ApplicationRecord
     recused_unassigned: 6
   }
 
-  STATUS_ORDER = %i[recused unassigned recused_unassigned not_started in_progress completed].freeze
-
   scope :ordered_by_status, lambda {
     order(
       Arel.sql(
-        [
-          "CASE evaluator_submission_assignments.status",
-          *STATUS_ORDER.map.with_index { |status, index| "WHEN #{statuses[status]} THEN #{index}" },
-          "ELSE #{STATUS_ORDER.length} END"
-        ].join(" ")
+        "CASE evaluator_submission_assignments.status
+          WHEN #{statuses[:recused]} THEN 0
+          WHEN #{statuses[:unassigned]} THEN 1
+          WHEN #{statuses[:recused_unassigned]} THEN 2
+          WHEN #{statuses[:not_started]} THEN 3
+          WHEN #{statuses[:in_progress]} THEN 4
+          WHEN #{statuses[:completed]} THEN 5
+          ELSE 6
+        END"
       )
     )
   }
