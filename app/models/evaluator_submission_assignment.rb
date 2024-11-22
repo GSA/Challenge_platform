d167e3529c6cb39d817e5b60076e89e41e8b1734
Chris Preisinger <cpreisinger@gmail.com>
2024-11-22 14:26:42 -0500

    230 Dependent destroy for assignment evaluations

--- app/models/evaluator_submission_assignment.rb
+++ app/models/evaluator_submission_assignment.rb
@@ -3,7 +3,7 @@
 class EvaluatorSubmissionAssignment < ApplicationRecord
   belongs_to :submission
   belongs_to :evaluator, class_name: "User", foreign_key: :user_id, inverse_of: :assigned_submissions
-  has_one :evaluation
+  has_one :evaluation, dependent: :destroy
 
   enum :status, { assigned: 0, unassigned: 1, recused: 2 }
 end
