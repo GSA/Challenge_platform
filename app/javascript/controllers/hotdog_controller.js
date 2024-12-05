a64f46e1d494d347248460e46449e61fe7c0adb6
Stone Filipczak <stone@smartlogic.io>
2024-12-05 11:04:31 -0500

    mobile layout

--- app/javascript/controllers/hotdog_controller.js
+++ app/javascript/controllers/hotdog_controller.js
@@ -2,21 +2,33 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="hotdog"
 export default class extends Controller {
-  static targets = ["rightPane", "collapseBar", "leftPane", "expandBar"];
+  static targets = ["rightPane", "leftPane", "hotdogCollapse", "hotdogExpand", "burgerCollapse", "burgerExpand", "burgerCollapsible"];
 
-  handleCollapse(e) {
+  hotdogCollapse(e) {
     this.rightPaneTarget.classList.add("display-none")
-    this.collapseBarTarget.classList.add("display-none")
-    this.expandBarTarget.classList.remove("display-none")
+    this.hotdogCollapseTarget.classList.add("display-none")
+    this.hotdogExpandTarget.classList.remove("display-none")
     this.leftPaneTarget.classList.add("width-full")
     this.leftPaneTarget.classList.remove("width-half")
   }
 
-  handleExpand(e) {
+  hotdogExpand(e) {
     this.rightPaneTarget.classList.remove("display-none")
-    this.collapseBarTarget.classList.remove("display-none")
-    this.expandBarTarget.classList.add("display-none")
+    this.hotdogCollapseTarget.classList.remove("display-none")
+    this.hotdogExpandTarget.classList.add("display-none")
     this.leftPaneTarget.classList.remove("width-full")
     this.leftPaneTarget.classList.add("width-half")
+  }
+
+  burgerCollapse(e) {
+    this.burgerCollapsibleTarget.classList.add("display-none")
+    this.burgerCollapseTarget.classList.add("display-none")
+    this.burgerExpandTarget.classList.remove("display-none")
+  }
+
+  burgerExpand(e) {
+    this.burgerCollapsibleTarget.classList.remove("display-none")
+    this.burgerCollapseTarget.classList.remove("display-none")
+    this.burgerExpandTarget.classList.add("display-none")
   }
 }
