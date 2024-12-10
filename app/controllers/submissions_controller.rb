3f1e16da6e0323bc425d841d2c8693eee207fd02
Stone Filipczak <stone@smartlogic.io>
2024-12-10 09:29:06 -0500

    disabling and verbage

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -9,7 +9,7 @@ class SubmissionsController < ApplicationController
 
   def update
     if @submission.update!(submission_params)
-      flash.now[:success] = I18n.t("comments_saved")
+      flash.now[:success] = I18n.t("submission_updated")
       render :show, submission: @submission
     else
       render :show, status: :unprocessable_entity, submission: @submission
