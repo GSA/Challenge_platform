1c0182cb7ff60ac4a66f0fcb2409d655ae66fde5
Chris Preisinger <cpreisinger@gmail.com>
2024-12-10 23:46:37 -0500

    237 PR feedback. Truncate tables, unused variable

--- db/migrate/20241125060011_add_unique_phase_constraint_to_evaluation_forms.rb
+++ db/migrate/20241125060011_add_unique_phase_constraint_to_evaluation_forms.rb
@@ -1,5 +1,12 @@
 class AddUniquePhaseConstraintToEvaluationForms < ActiveRecord::Migration[7.2]
   def change
+    ActiveRecord::Base.connection.truncate_tables(
+      EvaluationForm.table_name,
+      EvaluationCriterion.table_name,
+      Evaluation.table_name,
+      EvaluationScore.table_name
+    )
+
     remove_index :evaluation_forms, :phase_id, if_exists: true
     add_index :evaluation_forms, :phase_id, unique: true
   end
