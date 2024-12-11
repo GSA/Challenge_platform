a1ae489719a3df7d0689f4e9946bb6c86c741353
Chris Preisinger <cpreisinger@gmail.com>
2024-12-11 16:19:51 -0500

    [283] Eval Form: Cancelation modal (#315)
    * 283 Initial cancel modal for eval form
    
    * 283 Add accessibility test for cancel modal
    * 283 Change confirm/cancel modal to stimulus
    * 283 Allow modal actions. Confirm criteria removal
    
    ---------
    
    Co-authored-by: Stephen Chudleigh <stepchud@users.noreply.github.com>

---
+++ app/javascript/controllers/modal_controller.js
@@ -0,0 +1,87 @@
+import { Controller } from "@hotwired/stimulus";
+
+export default class extends Controller {
+  static targets = ["modal"];
+  static values = {
+    modalId: String,
+  };
+
+  open(event) {
+    const modalId = event.currentTarget.dataset.modalTargetId;
+    const modal = this.modalTargets.find((modal) => modal.id === modalId);
+
+    this.openEvent = event;
+
+    event.preventDefault();
+
+    if (modal) {
+      modal.showModal();
+    } else {
+      console.warn(`Modal with ID '${modalId}' not found.`);
+    }
+  }
+
+  confirm(event) {
+    const modal = this._getModal(event);
+
+    if (modal) {
+      const confirmRedirect = modal.dataset.modalConfirmRedirect;
+      const confirmAction = modal.dataset.modalConfirmAction;
+
+      if (confirmRedirect) {
+        window.location.href = confirmRedirect;
+      } else if (confirmAction) {
+        this.invokeAction(confirmAction);
+        modal.close();
+      } else {
+        modal.close();
+        return true;
+      }
+    }
+  }
+
+  cancel(event) {
+    const modal = this._getModal(event);
+
+    if (modal) {
+      const cancelRedirect = modal.dataset.modalCancelRedirect;
+      const cancelAction = modal.dataset.modalCancelAction;
+
+      if (cancelRedirect) {
+        window.location.href = cancelRedirect;
+      } else if (cancelAction) {
+        this.invokeAction(cancelAction);
+        modal.close();
+      } else {
+        modal.close();
+        return false;
+      }
+    }
+  }
+
+  _getModal(event) {
+    return event.currentTarget.closest("dialog");
+  }
+
+  invokeAction(actionName) {
+    const [controllerName, action] = actionName.split("#");
+    const controllerElement = document.querySelector(
+      `[data-controller~="${controllerName}"]`
+    );
+
+    if (!controllerElement) {
+      console.warn(`Controller element for ${controllerName} not found.`);
+    }
+
+    const controller = this.application.getControllerForElementAndIdentifier(
+      controllerElement,
+      controllerName
+    );
+
+    if (controller && typeof controller[action] === "function") {
+      controller[action](this.openEvent);
+    } else {
+      console.warn(`Action ${actionName} not found on ${controllerName}`);
+    }
+  }
+}
--- spec/system/evaluation_form_spec.rb
+++ spec/system/evaluation_form_spec.rb
@@ -16,6 +16,45 @@ RSpec.describe 'Evaluation Form', :js, type: :system do
       expect(page).to(be_axe_clean)
     end
 
+    it "shows a confirmation modal when clicking the cancel button" do
+      visit new_evaluation_form_path
+
+      click_link_or_button "Cancel"
+
+      assert_selector 'dialog#cancel', visible: true
+
+      expect(page).to(be_axe_clean)
+    end
+
+    it "redirects to evaluation form path when clicking yes in cancel modal" do
+      visit new_evaluation_form_path
+
+      click_link_or_button "Cancel"
+
+      assert_selector 'dialog#cancel', visible: true
+
+      within 'dialog#cancel' do
+        click_link_or_button 'Yes'
+      end
+
+      assert_current_path evaluation_forms_path
+    end
+
+    it "closes the cancel modal and does nothing if you click close" do
+      visit new_evaluation_form_path
+
+      click_link_or_button "Cancel"
+
+      assert_selector 'dialog#cancel', visible: true
+
+      within 'dialog#cancel' do
+        click_link_or_button 'Close'
+      end
+
+      assert_no_selector 'dialog#cancel', visible: true
+      assert_current_path new_evaluation_form_path
+    end
+
     it 'allows creation of a valid form with all 3 criteria scoring types' do
       visit new_evaluation_form_path
 
@@ -291,6 +330,45 @@ RSpec.describe 'Evaluation Form', :js, type: :system do
       expect(page).to(be_axe_clean)
     end
 
+    it "shows a confirmation modal when clicking the cancel button" do
+      visit edit_evaluation_form_path(evaluation_form)
+
+      click_link_or_button "Cancel"
+
+      assert_selector 'dialog#cancel', visible: true
+
+      expect(page).to(be_axe_clean)
+    end
+
+    it "redirects to evaluation form path when clicking yes in cancel modal" do
+      visit edit_evaluation_form_path(evaluation_form)
+
+      click_link_or_button "Cancel"
+
+      assert_selector 'dialog#cancel', visible: true
+
+      within 'dialog#cancel' do
+        click_link_or_button 'Yes'
+      end
+
+      assert_current_path evaluation_forms_path
+    end
+
+    it "closes the cancel modal and does nothing if you click close" do
+      visit edit_evaluation_form_path(evaluation_form)
+
+      click_link_or_button "Cancel"
+
+      assert_selector 'dialog#cancel', visible: true
+
+      within 'dialog#cancel' do
+        click_link_or_button 'Close'
+      end
+
+      assert_no_selector 'dialog#cancel', visible: true
+      assert_current_path edit_evaluation_form_path(evaluation_form)
+    end
+
     it 'allows editing of an existing form values' do
       visit edit_evaluation_form_path(evaluation_form)
 
@@ -377,7 +455,7 @@ RSpec.describe 'Evaluation Form', :js, type: :system do
       visit edit_evaluation_form_path(closed_evaluation_form)
 
       # Add expectation in spec to satisfy rubocop
-      expect(page).to have_css("form[data-controller='evaluation-form']")
+      expect(page).to have_css("form[data-controller='evaluation-form modal']")
       check_all_non_hidden_inputs_disabled_except_end_date
     end
   end
@@ -519,6 +597,12 @@ end
 
 def remove_criterion(index)
   click_link_or_button "evaluation_form_evaluation_criteria_attributes_#{index}_delete_criteria"
+
+  assert_selector 'dialog#remove-criteria', visible: true
+
+  within 'dialog#remove-criteria' do
+    click_link_or_button 'Yes'
+  end
 end
 
 def toggle_criteria_accordion(index)
@@ -791,7 +875,7 @@ end
 
 # Checks that all non hidden or end date fields are disabled
 def check_all_non_hidden_inputs_disabled_except_end_date
-  within("form[data-controller='evaluation-form']") do
+  within("form[data-controller='evaluation-form modal']") do
     all("input:not([type='hidden']), textarea, select").each do |field|
       if field[:id] == "evaluation_form_closing_date"
         expect(field).not_to be_disabled, "Expected #{field[:id]} to not be disabled"
