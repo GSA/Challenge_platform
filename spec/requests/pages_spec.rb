03c300b6d81a3f2e6dbf2291de930614ccf2e4b7
Stephen Chudleigh <stephen@smartlogic.io>
2024-12-17 10:14:22 -0600

    fix specs

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -1,8 +1,8 @@
 require "rails_helper"
 
 RSpec.describe "PagesController" do
-  it "get new renders successfully" do
-    stub_request(:get, PagesController::HOST + PagesController::BASE_URL).
+  it "get root renders successfully" do
+    stub_request(:get, PagesController::HOST + PagesController::BASE_URL + "/").
         to_return(status: 200, body: "", headers: {})
 
     get "/"
@@ -10,15 +10,15 @@ RSpec.describe "PagesController" do
   end
 
   it "removes full paths to assets so they proxy too" do
-    stub_request(:get, PagesController::HOST + PagesController::BASE_URL).
+    stub_request(:get, PagesController::HOST + PagesController::BASE_URL + "/").
         to_return(status: 200, body: PagesController::HOST + PagesController::BASE_URL, headers: {})
 
     get "/"
-    expect(response.body).to eq("//")
+    expect(response.body).to eq("/")
   end
 
   it "works for minified js assets" do
-    stub_request(:get, PagesController::HOST + PagesController::BASE_URL + "assets/uswds.min.js").
+    stub_request(:get, PagesController::HOST + PagesController::BASE_URL + "/assets/uswds.min.js").
         to_return(status: 200, body: "", headers: {})
 
     get "/assets/uswds.min.js"
@@ -26,7 +26,7 @@ RSpec.describe "PagesController" do
   end
 
   it "works for image assets" do
-    stub_request(:get, PagesController::HOST + PagesController::BASE_URL + "assets/logo.svg").
+    stub_request(:get, PagesController::HOST + PagesController::BASE_URL + "/assets/logo.svg").
         to_return(status: 200, body: "", headers: {})
 
     get "/assets/logo.svg"
@@ -34,7 +34,7 @@ RSpec.describe "PagesController" do
   end
 
   it "404s to the dashboard" do
-    stub_request(:get, PagesController::HOST + PagesController::BASE_URL + "not_found/").
+    stub_request(:get, PagesController::HOST + PagesController::BASE_URL + "/not_found/").
         to_return(status: 404, body: "", headers: {})
 
     get "/not_found"
