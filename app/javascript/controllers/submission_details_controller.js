1d11c916ffe91ea0a967f42ce5077c9b616baf85
Stone Filipczak <stone@smartlogic.io>
2024-12-17 11:20:35 -0500

    not working single form attempt

--- app/javascript/controllers/submission_details_controller.js
+++ app/javascript/controllers/submission_details_controller.js
@@ -2,21 +2,25 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="submission-details"
 export default class extends Controller {
-  static targets = ["qualifiedForm", "selectedForm"]
+  static targets = ["hiddenInput"]
   
   selectedSubmit(e) {
+    console.log(e.target.checked)
     if (e.target.checked) {
-      e.target.form.submit()
+      this.hiddenInputTarget.value = "selected"
+      e.target.setAttribute('checked', true);
     } else {
-      this.qualifiedFormTarget.submit()
+      this.hiddenInputTarget.setAttribute("value", "qualified")
+      e.target.setAttribute('checked', false);
     }
+    console.log(this.hiddenInputTarget)
   }
 
   winnerSubmit(e) {
     if (e.target.checked) {
-      e.target.form.submit()
+      this.hiddenInputTarget.value = "winner"
     } else {
-      this.selectedFormTarget.submit()
+      this.hiddenInputTarget.value = "selected"
     }
   }
 }
