ea655eb1e12645a2369e3b8a20b12cb6a87b8933
Chris Preisinger <cpreisinger@gmail.com>
2024-12-17 01:27:33 -0500

    296 Simplify validate presence function

--- app/javascript/controllers/evaluation_form_controller.js
+++ app/javascript/controllers/evaluation_form_controller.js
@@ -73,28 +73,39 @@ export default class extends Controller {
   validatePresence(e) {
     const target = e.target;
     const formGroup = target.closest(".usa-form-group");
-    const fieldName = target.dataset.fieldName;
+    const fieldName = target.dataset.fieldName || target.id;
 
+    const label = this.findLabel(target, formGroup);
+
+    if (!target.value) {
+      this.addErrorClasses(target, label);
+      this.updateErrorMessage(fieldName, "can't be blank");
+    } else {
+      this.removeErrorClasses(target, label);
+      this.updateErrorMessage(fieldName, "");
+    }
+  }
+
+  findLabel(target, formGroup) {
     const isSelect =
       target.tagName === "SELECT" ||
       target.classList.contains("usa-combo-box__input");
-
     const isRadio = target.type === "radio";
 
     const labelId = isSelect ? target.name : target.id;
     const labelQuery = isRadio ? "legend" : `label[for="${labelId}"]`;
 
-    const label = formGroup.querySelector(labelQuery);
+    return formGroup.querySelector(labelQuery);
+  }
 
-    if (!target.value) {
-      target.classList.add("border-secondary");
-      if (label) label.classList.add("text-secondary");
-      this.updateErrorMessage(fieldName || target.id, "can't be blank");
-    } else {
-      target.classList.remove("border-secondary");
-      if (label) label.classList.remove("text-secondary");
-      this.updateErrorMessage(fieldName || target.id, "");
-    }
+  addErrorClasses(target, label) {
+    target.classList.add("border-secondary");
+    if (label) label.classList.add("text-secondary");
+  }
+
+  removeErrorClasses(target, label) {
+    target.classList.remove("border-secondary");
+    if (label) label.classList.remove("text-secondary");
   }
 
   updateErrorMessage(field, message) {
