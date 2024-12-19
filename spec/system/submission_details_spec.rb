7f4a4807a7e3233fde46d46c3968f11057275b0d
Stone Filipczak <51140224+stonefilipczak@users.noreply.github.com>
2024-12-19 17:31:51 -0500

    [305] Submission Details - Judging Status and Comments (#318)
    * working single form approach
    * disabling checkboxes with javascript
    
    ---------
    
    Co-authored-by: Stephen Chudleigh <stepchud@users.noreply.github.com>
    Co-authored-by: Stephen Chudleigh <stephen@smartlogic.io>

---
+++ spec/system/submission_details_spec.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe "A11y", :js do
+  describe "Logged-in as a Challenge Manager" do
+    let(:user) { create_user(role: "challenge_manager") }
+    let(:challenge) { create_challenge(user: user, title: "Boston Tea Party Cleanup") }
+    let(:submission) { create(:submission, manager: user, challenge: challenge) }
+
+
+    before { system_login_user(user) }
+
+    it "submission details page is accessible" do
+      visit submission_path(submission)
+      expect(user.role).to eq("challenge_manager")
+      expect(page).to have_css('h1', text: "Submission ID #{submission.id}")
+      expect(page).to(be_axe_clean)
+    end
+
+    it "allows manipulation of judging status" do
+      visit submission_path(submission)
+
+      find_by_id('eligible-for-evaluation').click
+      click_on('Save')
+      updated_submission = Submission.find(submission.id)
+      expect(updated_submission.judging_status).to eq('selected')
+
+      find_by_id('selected-to-advance').click
+      click_on('Save')
+      updated_submission = Submission.find(submission.id)
+      expect(updated_submission.judging_status).to eq('winner')
+    end
+
+    it "saves comments" do
+      visit submission_path(submission)
+      comments = Faker::Lorem.sentence
+
+      fill_in "Comments and notes:", with: comments
+      click_on('Save')
+      updated_submission = Submission.find(submission.id)
+      expect(updated_submission.comments).to eq(comments)
+    end
+  end
+end
