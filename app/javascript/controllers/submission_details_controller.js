97d94df4bca5235513d728ecc1b93807fb40e93c
Stone Filipczak <stone@smartlogic.io>
2024-12-19 13:54:03 -0500

    disabling checkboxes with javascript

--- app/javascript/controllers/submission_details_controller.js
+++ app/javascript/controllers/submission_details_controller.js
@@ -5,19 +5,25 @@ export default class extends Controller {
   
   eligibleCheck(e) {
     const hiddenInput = e.target.form.elements["judging-status-hidden"]
+    const winnerCheckbox = e.target.form.elements[3]
     if (e.target.checked) {
       hiddenInput.value = "selected"
+      winnerCheckbox.disabled = false
     } else {
       hiddenInput.value = "qualified"
+      winnerCheckbox.disabled = true
     }
   }
 
   selectedCheck(e) {
     const hiddenInput = e.target.form.elements["judging-status-hidden"]
+    const eligibleCheckbox = e.target.form.elements[2]
     if (e.target.checked) {
       hiddenInput.value = "winner"
+      eligibleCheckbox.disabled = true
     } else {
       hiddenInput.value = "selected"
+      eligibleCheckbox.disabled = false
     }
   }
 }
