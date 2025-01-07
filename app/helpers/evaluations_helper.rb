52ed1e365e4d4ea2c527b7eccc9f9b1890357832
Stephen Chudleigh <stephen@smartlogic.io>
2025-01-06 16:14:11 -0800

    remove unused helper methods

--- app/helpers/evaluations_helper.rb
+++ app/helpers/evaluations_helper.rb
@@ -2,28 +2,4 @@
 
 # View helpers for calculating evaluation & submission details.
 module EvaluationsHelper
-  Score = Struct.new(:raw_score, :formatted_score, :display_score)
-
-  # individual evaluator score
-  def evaluator_score(assignment)
-    score = assignment.evaluation&.total_score
-
-    unless assignment.evaluation_status == :completed && score
-      return Score.new(0, "0", "N/A")
-    end
-
-    Score.new(score, score.to_s, score.to_s)
-  end
-
-  def average_score(submission)
-    completed_evaluations = submission.evaluations.where.not(completed_at: nil)
-
-    unless completed_evaluations.any?
-      return Score.new(0, "0", "N/A")
-    end
-
-    avg = completed_evaluations.average(:total_score)
-    score = avg ? avg.round : 0
-    Score.new(score, score.to_s, score.to_s)
-  end
 end
