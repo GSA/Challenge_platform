0be31fa8981c5ff12852d2b5802b8dd5eadf7f00
Emma Whamond <emma@smartlogic.io>
2025-01-07 13:47:17 -0600

    291 | Add format scoring

--- app/helpers/evaluations_helper.rb
+++ app/helpers/evaluations_helper.rb
@@ -2,4 +2,19 @@
 
 # View helpers for calculating evaluation & submission details.
 module EvaluationsHelper
+  Score = Struct.new(:raw_score, :formatted_score, :display_score)
+
+  def evaluator_score(assignment) # individual evaluator score
+    score = display_score(assignment)
+    return Score.new(0, "0", "N/A") if score == 'N/A'
+
+    Score.new(score, score.to_s, score.to_s)
+  end
+
+  def average_score(submission)
+    score = submission.average_score
+    return Score.new(0, "0", "N/A") if score.zero?
+
+    Score.new(score, score.to_s, score.to_s)
+  end
 end
