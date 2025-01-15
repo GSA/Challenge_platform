e631fa8ea8b3f1f68bb251a48f22941dd2c652d2
Emma Whamond <emma@smartlogic.io>
2025-01-14 18:31:36 -0600

    74 | Use evaluator invitation split_full_name

--- app/models/evaluator_invitation.rb
+++ app/models/evaluator_invitation.rb
@@ -25,10 +25,13 @@ class EvaluatorInvitation < ApplicationRecord
 
   validates :email, uniqueness: { scope: [:challenge_id, :phase_id] }
 
-  def full_name=(name)
+  def self.split_full_name(name)
     names = name.to_s.strip.split(/\s+/, 2)
-    self.first_name = names[0]
-    self.last_name = names[1]
+    [names[0], names[1]]
+  end
+
+  def full_name=(name)
+    self.first_name, self.last_name = self.class.split_full_name(name)
   end
 
   def full_name
