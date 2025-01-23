8a75f27d44107ba5b2977202c76ac7177dcc6342
Stone Filipczak <stone@smartlogic.io>
2025-01-23 15:42:14 -0500

    add schema info

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,5 +1,19 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: submission_documents
+#
+#  id                 :bigint           not null, primary key
+#  user_id            :bigint           not null
+#  submission_id      :bigint
+#  filename           :string(255)      not null
+#  uuid               :uuid             not null
+#  extension          :string(255)      not null
+#  name               :string(255)
+#  inserted_at        :datetime         not null
+#  updated_at         :datetime         not null
+#
 class Document < ApplicationRecord
   self.table_name = 'submission_documents'
 
@@ -7,7 +21,7 @@ class Document < ApplicationRecord
   belongs_to :submission
 
   def external_url
-    "#{ENV.fetch("PHOENIX_URI")}/uploads/documents/#{key}#{extension}"
+    "#{ENV.fetch('PHOENIX_URI')}/uploads/documents/#{key}#{extension}"
   end
 
   def display_name
