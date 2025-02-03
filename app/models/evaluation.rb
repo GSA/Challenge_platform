cd9ee1f27207a701a931404acf82d53af0a09a83
Chris Preisinger <cpreisinger@gmail.com>
2025-02-03 12:12:24 -0500

    [71] Evaluation Score Calculation (#375)
    * 71 Initial evaluation score calculation
    
    * 71 Fix issue with duplicate criterion scores
    * 71 Change rating and binary to use same rendering
    * 71 Fix issue with evaluation total score calc
    
    ---------
    
    Co-authored-by: Stephen Chudleigh <stepchud@users.noreply.github.com>

--- app/models/evaluation.rb
+++ app/models/evaluation.rb
@@ -25,6 +25,8 @@ class Evaluation < ApplicationRecord
   has_many :evaluation_criteria, through: :evaluation_form
   accepts_nested_attributes_for :evaluation_scores
 
+  validates_associated :evaluation_scores
+
   validates :user_id,
             uniqueness: { scope: [:evaluation_form_id, :submission_id],
                           message: I18n.t("evaluations.unique_user_for_evaluation_form_and_submission_error") }
@@ -40,11 +42,33 @@ class Evaluation < ApplicationRecord
 
   validate :user_has_valid_role
 
+  before_save :ensure_all_scores_exist
+  before_save :calculate_total_score
+
   private
 
   def user_has_valid_role
     return if User::VALID_EVALUATOR_ROLES.include?(user.role)
 
     errors.add(:user, "must have a valid evaluator role")
+  end
+
+  def ensure_all_scores_exist
+    missing_criteria =
+      evaluation_form.evaluation_criteria.where.not(id: evaluation_scores.map(&:evaluation_criterion_id))
+    missing_criteria.each do |criterion|
+      evaluation_scores.build(evaluation_criterion: criterion)
+    end
+  end
+
+  def calculate_total_score
+    return if evaluation_scores.blank?
+
+    # Ensure all scores have calculated values before summing
+    self.total_score = if evaluation_scores.any? { |score| score.calculated_score.blank? }
+                         nil
+                       else
+                         evaluation_scores.sum(&:calculated_score).round(2)
+                       end
   end
 end
