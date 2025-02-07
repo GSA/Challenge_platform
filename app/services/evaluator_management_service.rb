f3f383a70103327e366ca66fec8b9b82b1bd7199
Emma Whamond <emma@smartlogic.io>
2025-02-07 11:21:26 -0600

    342 | Extract evaluator and invitation removal into a service

--- app/services/evaluator_management_service.rb
+++ app/services/evaluator_management_service.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-# This service handles adding and removing evalutors to and from challenge phases.
+# This service handles inviting and adding evalutors to a challenge phase.
 class EvaluatorManagementService
   def initialize(challenge, phase)
     @challenge = challenge
@@ -15,14 +15,8 @@ class EvaluatorManagementService
   end
 
   def remove_evaluator(evaluator_type, evaluator_id)
-    case evaluator_type
-    when 'user'
-      remove_user_evaluator(evaluator_id)
-    when 'invitation'
-      remove_evaluator_invitation(evaluator_id)
-    else
-      { success: false, message: 'Invalid evaluator type' }
-    end
+    evaluator_removal_service = EvaluatorRemovalService.new(@challenge, @phase)
+    evaluator_removal_service.remove_evaluator(evaluator_type, evaluator_id)
   end
 
   def self.accept_evaluator_invitation(user)
@@ -98,54 +92,5 @@ class EvaluatorManagementService
     else
       { success: false, message: I18n.t('evaluators.process_evaluator_invitation.add_failure', email: user.email) }
     end
-  end
-
-  def remove_user_evaluator(evaluator_id)
-    ActiveRecord::Base.transaction do
-      evaluator = User.find(evaluator_id)
-      cpe = ChallengePhasesEvaluator.find_by(challenge: @challenge, phase: @phase, user: evaluator)
-
-      return evaluator_not_found_response unless cpe
-
-      delete_evaluator_assignments(evaluator)
-      delete_challenge_phase_evaluator(cpe)
-    end
-  rescue ActiveRecord::RecordNotFound
-    evaluator_not_found_response
-  rescue StandardError => e
-    { success: false, message: "Error: #{e.message}" }
-  end
-
-  def remove_evaluator_invitation(invitation_id)
-    invitation = @challenge.evaluator_invitations.find_by!(id: invitation_id, phase: @phase)
-    if invitation.destroy
-      { success: true, message: I18n.t('evaluators.remove_evaluator_invitation.success') }
-    else
-      { success: false, message: I18n.t('evaluators.remove_evaluator_invitation.failure') }
-    end
-  rescue ActiveRecord::RecordNotFound
-    { success: false, message: I18n.t('evaluators.remove_evaluator_invitation.invitation_not_found') }
-  rescue StandardError => e
-    { success: false, message: "Error: #{e.message}" }
-  end
-
-  # deletes each evaluator submission assignment and its associated evaluation
-  def delete_evaluator_assignments(evaluator)
-    EvaluatorSubmissionAssignment.where(
-      user_id: evaluator.id,
-      submission_id: @phase.submissions.select(:id)
-    ).destroy_all
-  end
-
-  def delete_challenge_phase_evaluator(cpe)
-    if cpe.destroy
-      { success: true, message: I18n.t('evaluators.remove_user_evaluator.success') }
-    else
-      { success: false, message: I18n.t('evaluators.remove_user_evaluator.failure') }
-    end
-  end
-
-  def evaluator_not_found_response
-    { success: false, message: I18n.t('evaluators.remove_user_evaluator.evaluator_not_found') }
   end
 end
---
+++ app/services/evaluator_removal_service.rb
@@ -0,0 +1,73 @@
+# frozen_string_literal: true
+
+# This service handles removing evalutors and evaluator invitations from a challenge phase.
+class EvaluatorRemovalService
+  def initialize(challenge, phase)
+    @challenge = challenge
+    @phase = phase
+  end
+
+  def remove_evaluator(evaluator_type, evaluator_id)
+    case evaluator_type
+    when 'user'
+      remove_user_evaluator(evaluator_id)
+    when 'invitation'
+      remove_evaluator_invitation(evaluator_id)
+    else
+      { success: false, message: 'Invalid evaluator type' }
+    end
+  end
+
+  private
+
+  def remove_user_evaluator(evaluator_id)
+    ActiveRecord::Base.transaction do
+      evaluator = User.find(evaluator_id)
+      cpe = ChallengePhasesEvaluator.find_by(challenge: @challenge, phase: @phase, user: evaluator)
+
+      if cpe
+        delete_evaluator_assignments(evaluator)
+        delete_challenge_phase_evaluator(cpe)
+      else
+        next evaluator_not_found_response
+      end
+    end
+  rescue ActiveRecord::RecordNotFound
+    evaluator_not_found_response
+  rescue StandardError => e
+    { success: false, message: "Error: #{e.message}" }
+  end
+
+  def remove_evaluator_invitation(invitation_id)
+    invitation = @challenge.evaluator_invitations.find_by!(id: invitation_id, phase: @phase)
+    if invitation.destroy
+      { success: true, message: I18n.t('evaluators.remove_evaluator_invitation.success') }
+    else
+      { success: false, message: I18n.t('evaluators.remove_evaluator_invitation.failure') }
+    end
+  rescue ActiveRecord::RecordNotFound
+    { success: false, message: I18n.t('evaluators.remove_evaluator_invitation.invitation_not_found') }
+  rescue StandardError => e
+    { success: false, message: "Error: #{e.message}" }
+  end
+
+  # deletes each evaluator submission assignment and its associated evaluation
+  def delete_evaluator_assignments(evaluator)
+    EvaluatorSubmissionAssignment.where(
+      user_id: evaluator.id,
+      submission_id: @phase.submissions.select(:id)
+    ).destroy_all
+  end
+
+  def delete_challenge_phase_evaluator(cpe)
+    if cpe.destroy
+      { success: true, message: I18n.t('evaluators.remove_user_evaluator.success') }
+    else
+      { success: false, message: I18n.t('evaluators.remove_user_evaluator.failure') }
+    end
+  end
+
+  def evaluator_not_found_response
+    { success: false, message: I18n.t('evaluators.remove_user_evaluator.evaluator_not_found') }
+  end
+end
