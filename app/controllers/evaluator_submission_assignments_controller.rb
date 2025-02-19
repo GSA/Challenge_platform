714525a92129eddbf2a32c109261b36322a08ca2
Emma Whamond <emma@smartlogic.io>
2025-02-19 17:57:55 -0600

    394 | Update submissions stats dashboards

--- app/controllers/evaluator_submission_assignments_controller.rb
+++ app/controllers/evaluator_submission_assignments_controller.rb
@@ -17,6 +17,9 @@ class EvaluatorSubmissionAssignmentsController < ApplicationController
     @unassigned_submissions = @evaluator_assignments.
       where(status: %i[unassigned recused_unassigned]).
       ordered_by_status
+    @recused_submissions = @evaluator_assignments.
+      where(status: %i[recused]).
+      ordered_by_status
     @submissions_count = helpers.calculate_submissions_count(@assigned_submissions)
   end
 
