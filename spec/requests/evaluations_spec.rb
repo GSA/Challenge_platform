8730f2ddebdddfb2e9e9dbc8e53afed711e82b88
Emma Whamond <emma@smartlogic.io>
2025-02-19 19:16:09 -0600

    394 | Update specs related to submissions stats dashboard

--- spec/requests/evaluations_spec.rb
+++ spec/requests/evaluations_spec.rb
@@ -216,13 +216,14 @@ RSpec.describe "Evaluations" do
 
           get submissions_evaluation_path(phase)
 
-          expect(response.body).to include("Completed")
-          expect(response.body).to include("In Progress")
-          expect(response.body).to include("Not Started")
+          expect(response.body).to include("COMPLETED")
+          expect(response.body).to include("IN PROGRESS")
+          expect(response.body).to include("NOT")
+          expect(response.body).to include("STARTED")
 
-          expect(response.body).to include('<span class="font-sans-xl text-success-dark text-bold display-block">1</span>')
-          expect(response.body).to include('<span class="font-sans-xl text-accent-warm-dark text-bold display-block">1</span>')
-          expect(response.body).to include('<span class="font-sans-xl text-error-dark text-bold display-block">1</span>')
+          expect(response.body).to have_css(".bg-green-cool-vivid-60v .font-sans-xl.text-white.text-bold", text: "1")
+          expect(response.body).to have_css(".bg-orange-warm-vivid-50v .font-sans-xl.text-white.text-bold", text: "1")
+          expect(response.body).to have_css(".bg-red-vivid-60v .font-sans-xl.text-white.text-bold", text: "1")
         end
       end
 
