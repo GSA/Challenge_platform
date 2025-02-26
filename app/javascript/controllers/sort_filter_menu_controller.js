26f0053f7749ca1da6a73d19abe742b0667c3654
Emma Whamond <emma@smartlogic.io>
2025-02-26 17:43:30 -0600

    400 | Add functionality to handle sort and filtering with radio buttons

--- app/javascript/controllers/sort_filter_menu_controller.js
+++ app/javascript/controllers/sort_filter_menu_controller.js
@@ -2,59 +2,78 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["menuItem"]
+  static targets = ["menuItem", "filterOption"]
 
   connect() {
-    this.updateCheckmarks()
+    this.searchTerm = ''
+    this.setInitialFilterState()
   }
 
-  toggle(event) {
-    event.preventDefault()
-    const clickedItem = event.currentTarget
-    const filterValue = clickedItem.dataset.filterValue
-    
+  setInitialFilterState() {
     const currentUrl = new URL(window.location.href)
-    const [param, value] = filterValue.split('=')
-    
-    if (currentUrl.searchParams.get(param) === value) {
-      this.clearAllFilters()
-    } else {
-      this.applyFilter(filterValue)
-    }
-  }
 
-  applyFilter(filterValue) {
-    const currentUrl = new URL(window.location.href)
-    const [param, value] = filterValue.split('=')
-  
-    this.removeExistingParams(currentUrl)
-    currentUrl.searchParams.set(param, value)
-    window.location.href = currentUrl.toString()
-  }
+    this.filterOptionTargets.forEach(option => option.checked = false)
 
-  clearAllFilters() {
-    const currentUrl = new URL(window.location.href)
-    this.removeExistingParams(currentUrl)
-    window.location.href = currentUrl.toString()
-  }
-
-  removeExistingParams(url) {
-    const paramsToRemove = ['status', 'eligible_for_evaluation', 'selected_to_advance', 'sort']
-    paramsToRemove.forEach(key => {
-      url.searchParams.delete(key)
-    })
-  }
-
-  updateCheckmarks() {
-    const currentUrl = new URL(window.location.href)
-    this.menuItemTargets.forEach(item => {
-      const checkmark = item.querySelector('.checkmark')
-      const [param, value] = item.dataset.filterValue.split('=')
+    this.filterOptionTargets.forEach(option => {
+      const [param, value] = option.value.split('=')
       if (currentUrl.searchParams.get(param) === value) {
-        checkmark.style.display = 'inline'
-      } else {
-        checkmark.style.display = 'none'
+        option.checked = true
+
+        this.filterOptionTargets.forEach(otherOption => {
+          if (otherOption !== option && otherOption.checked) {
+            otherOption.checked = false
+          }
+        })
       }
     })
+  }
+
+  handleSearchInput(event) {
+    this.searchTerm = event.target.value.trim()
+  }
+
+  handleRadioChange(event) {
+    const clickedRadio = event.target
+
+    this.filterOptionTargets.forEach(radio => {
+      if (radio !== clickedRadio) {
+        radio.checked = false
+      }
+    })
+  }
+
+  applyFilters() {
+    const selectedFilters = this.filterOptionTargets
+      .filter(radio => radio.checked)
+      .map(radio => radio.value)
+      .join('&')
+
+    let queryParams = selectedFilters
+
+    if (this.searchTerm) {
+      queryParams = queryParams ? 
+        `${queryParams}&submission_id=${this.searchTerm}` :
+        `submission_id=${this.searchTerm}`
+    }
+
+    window.location.href = `${window.location.pathname}?${queryParams}`
+  }
+  
+  clearAllFilters() {
+    if (this.hasSubmissionIdSearchTarget) {
+      this.submissionIdSearchTarget.value = ''
+      this.searchTerm = ''
+    }
+    
+    this.filterOptionTargets.forEach(radio => {
+      radio.checked = false
+    })
+
+    window.location.href = window.location.pathname
+  }
+
+  close() {
+    const button = document.querySelector('[aria-controls="sort-and-filter"]')
+    button.click()
   }
 }
