bf5f850e0e6683579d941f6fd9ca0b694424fb86
Stephen Chudleigh <stephen.chudleigh@gmail.com>
2025-03-04 11:18:50 -0800

    fix system spec

--- spec/system/evaluator_dashboard_spec.rb
+++ spec/system/evaluator_dashboard_spec.rb
@@ -5,34 +5,50 @@ RSpec.describe 'Evaluator Dashboard', :js, type: :system do
   let(:challenge) { create(:challenge) }
   let(:phase) { create(:phase, challenge:) }
   let(:submission) { create(:submission, challenge:, phase:) }
-  let!(:evaluation_form) do
+  let(:evaluation_form) do
     create(:evaluation_form, :pointed, challenge:, phase:, evaluation_criteria_attrs: [
              { title: "Criterion 1", points_or_weight: 50, scoring_type: :numeric },
              { title: "Criterion 2", points_or_weight: 25, scoring_type: :binary },
              { title: "Criterion 3", points_or_weight: 25, scoring_type: :rating }
            ])
   end
-  let(:challenge_phases_evaluator) do
-    create(:challenge_phases_evaluator, challenge:, phase:, user: evaluator)
+
+  context "without any assigned evaluations" do
+    before do
+      system_login_user(evaluator)
+    end
+
+    it 'is accessible' do
+      visit evaluations_path
+
+      expect(page).to have_css('h1', text: "My Evaluations")
+      expect(page).to have_css('p', text: "You currently do not have any challenges.")
+      expect(page).to be_axe_clean
+    end
   end
-  let!(:assignment) { create(:evaluator_submission_assignment, evaluator:, submission:) }
 
-  before do
-    system_login_user(evaluator)
-  end
+  context "with an assigned evaluation" do
+    before do
+      evaluation_form
+      create(:challenge_phases_evaluator, challenge:, phase:, user: evaluator)
+      create(:evaluator_submission_assignment, evaluator:, submission:)
+      system_login_user(evaluator)
+    end
 
-  it 'is accessible' do
-    visit evaluations_path
+    it 'is accessible' do
+      visit evaluations_path
 
-    expect(page).to be_axe_clean
-  end
+      expect(page).to be_axe_clean
+    end
 
-  it 'contains the correct content' do
-    visit evaluations_path
+    it 'contains the correct content' do
+      visit evaluations_path
 
-    expect(page).to have_content(challenge_phase_title(challenge, phase))
-    expect(page).to have_content("1 of 1 Submissions")
-    expect(page).to have_content(evaluation_form.closing_date.strftime("%m/%d/%Y"))
-    expect(page).to have_content(evaluator_evaluation_status(evaluator, phase).to_s.titleize.upcase)
+      expect(page).to have_css('h1', text: "My Evaluations")
+      expect(page).to have_css('th[data-label="Challenge Title"]', text: challenge_phase_title(challenge, phase))
+      expect(page).to have_css('td[data-label="# of Submissions to Evaluate"]', text: "1 of 1 Submissions")
+      expect(page).to have_css('td[data-label="Evaluation Due Date"]', text: evaluation_form.closing_date.strftime("%m/%d/%Y"))
+      expect(page).to have_css('td[data-label="Evaluation Status"]', text: evaluator_evaluation_status(evaluator, phase).to_s.titleize.upcase)
+    end
   end
 end
