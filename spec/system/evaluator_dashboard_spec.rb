76c939d7fd2ee6cc199e4930916baaaf75820b90
Stephen Chudleigh <stephen.chudleigh@gmail.com>
2025-03-05 15:21:34 -0800

    fix flapper

--- spec/system/evaluator_dashboard_spec.rb
+++ spec/system/evaluator_dashboard_spec.rb
@@ -28,10 +28,13 @@ RSpec.describe 'Evaluator Dashboard', :js, type: :system do
   end
 
   context "with an assigned evaluation" do
+    let(:esa) { create(:evaluator_submission_assignment, :assigned, evaluator:, submission:) }
+    let(:cpe) { create(:challenge_phases_evaluator, challenge:, phase:, user: evaluator) }
+
     before do
       evaluation_form
-      create(:challenge_phases_evaluator, challenge:, phase:, user: evaluator)
-      create(:evaluator_submission_assignment, evaluator:, submission:)
+      cpe
+      esa
       system_login_user(evaluator)
     end
 
@@ -42,13 +45,34 @@ RSpec.describe 'Evaluator Dashboard', :js, type: :system do
     end
 
     it 'contains the correct content' do
+      phase_title = challenge_phase_title(challenge, phase)
       visit evaluations_path
 
       expect(page).to have_css('h1', text: "My Evaluations")
-      expect(page).to have_css('th[data-label="Challenge Title"]', text: challenge_phase_title(challenge, phase))
+      expect(page).to have_css('th[data-label="Challenge Title"]', text: phase_title)
       expect(page).to have_css('td[data-label="# of Submissions to Evaluate"]', text: "1 of 1 Submissions")
       expect(page).to have_css('td[data-label="Evaluation Due Date"]', text: evaluation_form.closing_date.strftime("%m/%d/%Y"))
       expect(page).to have_css('td[data-label="Evaluation Status"]', text: evaluator_evaluation_status(evaluator, phase).to_s.titleize.upcase)
+    end
+
+    it 'contains the correct content after being recused' do
+      phase_title = challenge_phase_title(challenge, phase)
+      esa.update(status: :recused)
+      visit evaluations_path
+
+      expect(page).to have_css('h1', text: "My Evaluations")
+      expect(page).to have_css('th[data-label="Challenge Title"]', text: phase_title)
+      expect(page).to have_css('td[data-label="# of Submissions to Evaluate"]', text: "1 of 1 Submissions")
+      expect(page).to have_css('td[data-label="Evaluation Due Date"]', text: evaluation_form.closing_date.strftime("%m/%d/%Y"))
+      expect(page).to have_css('td[data-label="Evaluation Status"]', text: evaluator_evaluation_status(evaluator, phase).to_s.titleize.upcase)
+    end
+
+    it 'contains the correct content after being unassigned' do
+      esa.update(status: :unassigned)
+      visit evaluations_path
+
+      expect(page).to have_css('h1', text: "My Evaluations")
+      expect(page).to have_css('p', text: "You currently do not have any challenges.")
     end
   end
 end
