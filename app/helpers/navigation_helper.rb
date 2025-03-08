2094fd70f88c5e00859769c31bdc36198ef26914
Stephen Chudleigh <stephen.chudleigh@gmail.com>
2025-03-07 20:52:21 -0800

    codeclimate

--- app/helpers/navigation_helper.rb
+++ app/helpers/navigation_helper.rb
@@ -8,14 +8,18 @@ module NavigationHelper
                    "margin-x-3 text-white text-bold text-no-underline " \
                    "tablet:width-auto tablet:text-no-wrap text-center",
             id: "utility-menu-link-#{button_label}") do
-      concat image_tag(
-        "images/usa-icons/#{image_path}.svg",
-        class: "usa-icon--size-4 icon-white margin-right-1",
-        alt: ""
+      concat(
+        image_tag(
+          "images/usa-icons/#{image_path}.svg",
+          class: "usa-icon--size-4 icon-white margin-right-1",
+          alt: ""
+        )
       )
-      concat tag.span(
-        button_label,
-        class: "text-white"
+      concat(
+        tag.span(
+          button_label,
+          class: "text-white"
+        )
       )
     end
   end
