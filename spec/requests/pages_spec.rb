d4887720cdcc8185619b8b914c826b11e1011b05
Stephen Chudleigh <stephen.chudleigh@gmail.com>
2025-04-02 12:48:13 -0700

    fix pages_spec for local dev

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -9,12 +9,18 @@ RSpec.describe "PagesController" do
     expect(response).to be_ok
   end
 
-  it "removes full paths to assets so they proxy too" do
+  it "replaces full paths from body to assets so they proxy too" do
+    body = "BEGIN_#{PagesController::HOST}#{PagesController::BASE_URL}_END"
     stub_request(:get, "#{PagesController::HOST}#{PagesController::BASE_URL}/").
-      to_return(status: 200, body: PagesController::HOST + PagesController::BASE_URL, headers: {})
+      to_return(status: 200, body:, headers: {})
 
     get "/"
-    expect(response.body).to eq("/")
+    expected_body = if PagesController::BASE_URL == "/"
+      "BEGIN_//_END"
+    else
+      "BEGIN_/_END"
+    end
+    expect(response.body).to eq(expected_body)
   end
 
   it "works for minified js assets" do
