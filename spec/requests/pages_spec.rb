e03ebd1b5fdff56cc813a31672bf6e9069b40d73
Stephen Chudleigh <stepchud@users.noreply.github.com>
2025-04-04 10:43:49 -0600

    fix pages_spec for local dev (#495)
    * fix pages_spec for local dev

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -1,39 +1,49 @@
 require "rails_helper"
 
 RSpec.describe "PagesController" do
-  it "get root renders successfully" do
-    stub_request(:get, "#{PagesController::HOST}#{PagesController::BASE_URL}/").
-      to_return(status: 200, body: "", headers: {})
+  context "#root" do
+    it "renders successfully" do
+      stub_request(:get, "#{PagesController::HOST}#{PagesController::BASE_URL}/").
+        to_return(status: 200, body: "", headers: {})
 
-    get "/"
-    expect(response).to be_ok
+      get "/"
+      expect(response).to be_ok
+    end
+
+    it "converts full paths in the response body with local paths so they proxy too" do
+      body = "BEGIN_#{PagesController::HOST}#{PagesController::BASE_URL}_END"
+      stub_request(:get, "#{PagesController::HOST}#{PagesController::BASE_URL}/").
+        to_return(status: 200, body:, headers: {})
+
+      get "/"
+      expected_body = if PagesController::BASE_URL == "/"
+        "BEGIN_//_END"
+      else
+        "BEGIN_/_END"
+      end
+      expect(response.body).to eq(expected_body)
+    end
   end
 
-  it "removes full paths to assets so they proxy too" do
-    stub_request(:get, "#{PagesController::HOST}#{PagesController::BASE_URL}/").
-      to_return(status: 200, body: PagesController::HOST + PagesController::BASE_URL, headers: {})
+  context "#assets" do
+    it "works for minified js assets" do
+      stub_request(:get, "#{PagesController::HOST}#{PagesController::BASE_URL}/assets/uswds.min.js").
+        to_return(status: 200, body: "", headers: {})
 
-    get "/"
-    expect(response.body).to eq("/")
+      get "/assets/uswds.min.js"
+      expect(response).to be_ok
+    end
+
+    it "works for image assets" do
+      stub_request(:get, "#{PagesController::HOST}#{PagesController::BASE_URL}/assets/logo.svg").
+        to_return(status: 200, body: "", headers: {})
+
+      get "/assets/logo.svg"
+      expect(response).to be_ok
+    end
   end
 
-  it "works for minified js assets" do
-    stub_request(:get, "#{PagesController::HOST}#{PagesController::BASE_URL}/assets/uswds.min.js").
-      to_return(status: 200, body: "", headers: {})
-
-    get "/assets/uswds.min.js"
-    expect(response).to be_ok
-  end
-
-  it "works for image assets" do
-    stub_request(:get, "#{PagesController::HOST}#{PagesController::BASE_URL}/assets/logo.svg").
-      to_return(status: 200, body: "", headers: {})
-
-    get "/assets/logo.svg"
-    expect(response).to be_ok
-  end
-
-  it "404s to the root path" do
+  it "404 redirects to the root path" do
     stub_request(:get, "#{PagesController::HOST}#{PagesController::BASE_URL}/not_found/").
       to_return(status: 404, body: "", headers: {})
 
