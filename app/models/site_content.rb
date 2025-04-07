0d8de5342ac79ab3dace288501c5ffad4c7780b2
Stephen Chudleigh <stephen.chudleigh@gmail.com>
2025-04-07 16:44:59 -0700

    implement the site_wide_banner

--- app/models/site_content.rb
+++ app/models/site_content.rb
@@ -13,4 +13,26 @@
 #
 class SiteContent < ApplicationRecord
   self.table_name = 'site_content'
+
+  def self.site_wide_banner
+    find_by(section: 'site_wide_banner')
+  end
+
+  # Used by temporary content such as the "site_wide_banner"
+  # This is used to determine if the content should be displayed.
+  # Returns true if the content is present and current.
+  # `current` = current time is between the start_date and end_date.
+  #
+  # @return [Boolean] true if the content is active, false otherwise.
+  #
+  # @example
+  #   content = SiteContent.new(start_date: DateTime.current - 1.day, end_date: DateTime.current + 1.day)
+  #   content.active? # => true
+  def active?
+    return false if start_date.nil? || end_date.nil?
+    return false if content.blank?
+
+    now = DateTime.current
+    start_date <= now && end_date >= now
+  end
 end
--- spec/models/site_content_spec.rb
+++ spec/models/site_content_spec.rb
@@ -12,5 +12,41 @@
 require 'rails_helper'
 
 RSpec.describe SiteContent, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe ".site_wide_banner" do
+    it "returns the site_wide_banner content" do
+      content = SiteContent.create(section: "site_wide_banner", content: "<p>Test Content</p>")
+      expect(SiteContent.site_wide_banner).to eq(content)
+    end
+    it "returns nil if no site_wide_banner content exists" do
+      SiteContent.create(section: "other_section", content: "Test other section content")
+      expect(SiteContent.site_wide_banner).to be_nil
+    end
+  end
+
+  describe "#active?" do
+    it "returns false if start_date is nil" do
+      content = SiteContent.new(start_date: nil, end_date: DateTime.current + 1.day)
+      expect(content.active?).to be false
+    end
+    it "returns false if end_date is nil" do
+      content = SiteContent.new(start_date: DateTime.current - 1.day, end_date: nil)
+      expect(content.active?).to be false
+    end
+    it "returns false if content is blank" do
+      content = SiteContent.new(start_date: DateTime.current - 1.day, end_date: DateTime.current + 1.day, content: nil)
+      expect(content.active?).to be false
+    end
+    it "returns false if current time is before start_date" do
+      content = SiteContent.new(start_date: DateTime.current + 1.day, end_date: DateTime.current + 2.days)
+      expect(content.active?).to be false
+    end
+    it "returns false if current time is after end_date" do
+      content = SiteContent.new(start_date: DateTime.current - 2.days, end_date: DateTime.current - 1.day)
+      expect(content.active?).to be false
+    end
+    it "returns true if current time is between start_date and end_date" do
+      content = SiteContent.new(start_date: DateTime.current - 1.day, end_date: DateTime.current + 1.day, content: "<h1>Test Content</h1>")
+      expect(content.active?).to be true
+    end
+  end
 end
